1c0e2a6a83e51006bb4ff9a3da3fcb5fa767bbc4
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-03-16 01:24:00 +0300

    Queries in sql

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', NULL, 'M', 'Brown', 'Friendly', true),
+  ('Rowdy', 5, 'M', 'Black', 'Aggressive', true),
+  ('Tabitha', 3, 'F', 'Gray', 'Playful', true),
+  ('Sergeant Brown', 8, 'M', 'Brown', 'Disciplined', true),
+  ('Melissa', 6, 'F', 'White', 'Calm', true),
+  ('Grinch', 10, 'M', 'Green', 'Grumpy', true),
+  ('Wendy', 4, 'F', 'Brown', 'Energetic', true),
+  (NULL, NULL, NULL, NULL, NULL, NULL);
